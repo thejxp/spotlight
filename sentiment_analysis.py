964d6b696a1d3b536948d2a0766c1f61c8f5640c
Alisha Mithal <alisha.mithal@gmail.com>
2017-10-11 01:57:33 -0400

    Add files via upload

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,4 +1,4 @@
-# TWEET SENTIMENT ANALYSIS 
+# TWEET SENTIMENT ANALYSIS
 
 import nltk
 
@@ -10,32 +10,28 @@ import datetime
 from data import *
 
 
-class MachineSpotlight(object): 
-
-    ########################################################################################################################
-    #  DATA STORAGE AND MANAGEMENT                                                                                         #
-    ########################################################################################################################
-
+class MachineSpotlight(object):
+    
     def manage_data(self):
         """Re-initializes ID_array and rated_tweets based on
-        imported values"""
+            imported values"""
         self.rated_tweets = rated_tweets
-
+        
         self.positive_array = []
         self.negative_array = []
         self.neutral_array = []
-
+        
         self.positive_dictionary = positive_dict
         self.negative_dictionary = negative_dict
         self.neutral_dictionary = neutral_dict
         self.start_index = start_tweets
-        
+    
     
     def write_data(self, file_name, message="No name given"):
         """ Writes data to file (appends; doesn't delete or override)
-        
-        Optional parameter: message defaults to "No name given"
-        """
+            
+            Optional parameter: message defaults to "No name given"
+            """
         
         ts = time.time()
         timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -50,42 +46,42 @@ class MachineSpotlight(object):
             
             myfile.write("\n\npositive_dict = ")
             myfile.write(`self.positive_dictionary`)
-           
+            
             myfile.write("\n\nnegative_dict = ")
             myfile.write(`self.negative_dictionary`)
             
             myfile.write("\n\nneutral_dict = ")
             myfile.write(`self.neutral_dictionary`)
-            
-                   
-                        
-    def build_classifier(self):
-                
-        self.make_three_arrays()
+
+
+
+def build_classifier(self):
+    
+    self.make_three_arrays()
         
         
         self.get_frequency_in_each(self.positive_array, self.positive_dictionary)
         self.get_frequency_in_each(self.negative_array, self.negative_dictionary)
         self.get_frequency_in_each(self.neutral_array, self.neutral_dictionary)
-        
     
-   
+    
+    
     def update_classifier(self, new_tweet):
         """Input: String representing a tweet
-        
-        Returns: a string representation of the score
-        """
             
+            Returns: a string representation of the score
+            """
+        
         tweet_score = self.get_score(new_tweet)
-        self.update_dictionaries(new_tweet, tweet_score)    
+        self.update_dictionaries(new_tweet, tweet_score)
         return tweet_score
     
     
     def make_three_arrays(self):
         """
-        Output: Three arrays are created with a list of meaningful words
-        """
-                
+            Output: Three arrays are created with a list of meaningful words
+            """
+        
         for i in range(self.start_index, len(self.rated_tweets)):
             words = self.rated_tweets[i][1]
             rating = self.rated_tweets[i][2]
@@ -101,10 +97,10 @@ class MachineSpotlight(object):
     
     def get_frequency_in_each(self, some_array, dictionary):
         """Input: a wordlist, one of the three arrays from above..., a dictionary to write to
+            
+            Output: NONE ---\\\ignore ---- a dictionarys with format key: value being "word": # of occurences
+            """
         
-        Output: NONE ---\\\ignore ---- a dictionarys with format key: value being "word": # of occurences
-        """
-
         
         for key in some_array:
             if key not in dictionary:
@@ -112,10 +108,10 @@ class MachineSpotlight(object):
             else:
                 dictionary[key] += 1
 
+
+def get_score(self, new_tweet):
     
-    def get_score(self, new_tweet):
-        
-        percentage = ''
+    percentage = ''
         score = 0
         ## CAST new_tweet TO UNICODE FORM
         
@@ -138,30 +134,30 @@ class MachineSpotlight(object):
                 score += float((pos-neg)/(pos+neg+neu))
             except ZeroDivisionError:
                 score = 0.0
-        try:
-            score = score/(len(tokens)*2)
+    try:
+        score = score/(len(tokens)*2)
         except:
             score = 0.0
-        
-        if score > 0:
-            percentage = 50 - int((score * 100) + 0.5) / 100.0
+
+    if score > 0:
+        percentage = 50 - int((score * 100) + 0.5) / 100.0
             return (str(percentage) + '% Negative (Unlikely)')
         
         elif score == 0:
             percentage = 50 - int((score * 100) + 0.5) / 100.0
             
             return (str(percentage) + '% Negative (Medium Likelihood)')
+
+else:
+    percentage = 50 - int((abs(score) * 100) + 0.5) / 100.0
+        
+        return (str(percentage) + '% Negative (Likely)')
     
-        else:
-            percentage = 50 - int((abs(score) * 100) + 0.5) / 100.0
-            
-            return (str(percentage) + '% Negative (Likely)')    
-        
     def update_dictionaries(self, new_tweet, score):
-        """Input: tweet 
+        """Input: tweet
+            
+            """
         
-        """
-                
         tokens = nltk.word_tokenize(new_tweet)
         
         for key in tokens:
@@ -181,11 +177,10 @@ class MachineSpotlight(object):
                 else:
                     self.negative_dictionary[key] += 1
 
-    
-        
-    
-    
-    
+
+
+
+
 
 
 
